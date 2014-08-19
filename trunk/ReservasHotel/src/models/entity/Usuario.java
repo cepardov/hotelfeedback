8bdbafa2f5a93683c027d39ef813d1d8c37a3274
pablo.santana.r@gmail.com <pablo.santana.r@gmail.com@878a9883-f9bb-5e60-a21f-57833b7b4551>
2014-08-19 00:24:39 +0000

    inicio

---
+++ trunk/ReservasHotel/src/models/entity/Usuario.java
@@ -0,0 +1,79 @@
+package models.entity;
+
+
+public class Usuario {
+    protected String rutusuario;
+    protected String nombre;
+    protected String paterno;
+    protected String materno;
+    protected String telefono;
+    protected String mail;
+    protected String privilegio;
+    protected String clave;
+
+    public String getRutusuario() {
+        return rutusuario;
+    }
+
+    public void setRutusuario(String rutusuario) {
+        this.rutusuario = rutusuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPaterno() {
+        return paterno;
+    }
+
+    public void setPaterno(String paterno) {
+        this.paterno = paterno;
+    }
+
+    public String getMaterno() {
+        return materno;
+    }
+
+    public void setMaterno(String materno) {
+        this.materno = materno;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPrivilegio() {
+        return privilegio;
+    }
+
+    public void setPrivilegio(String privilegio) {
+        this.privilegio = privilegio;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    
+}
---
+++ trunk/ReservasHotel/src/vistas/reservashotel/ReservasHotel.java
@@ -0,0 +1,12 @@
+package vistas.reservashotel;
+import utilidades.conexionbd;
+
+public class ReservasHotel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        conexionbd.getInstanceConnection();
+    }
+}
