f6b34b3adda163ebe64c9eec38cd15716583f22c
cepardov@gmail.com <cepardov@gmail.com@878a9883-f9bb-5e60-a21f-57833b7b4551>
2014-08-19 15:27:39 +0000

    Nueva clase de conexion a base de datos

---
+++ Hotel/src/models/entity/Usuario.java
@@ -0,0 +1,72 @@
+package models.entity;
+
+public class Usuario {
+    protected String rut;
+    protected String nombre;
+    protected String paterno;
+    protected String materno;
+    protected int privilegio;
+    protected String usuario;
+    protected String clave;
+
+    public void prueba(){
+        System.out.println("prueba:"+rut);
+    }
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPaterno() {
+        return paterno;
+    }
+
+    public void setPaterno(String paterno) {
+        this.paterno = paterno;
+    }
+
+    public String getMaterno() {
+        return materno;
+    }
+
+    public void setMaterno(String materno) {
+        this.materno = materno;
+    }
+
+    public int getPrivilegio() {
+        return privilegio;
+    }
+
+    public void setPrivilegio(int privilegio) {
+        this.privilegio = privilegio;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    
+}
