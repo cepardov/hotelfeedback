2d53bbf2b165b235a2f6c47c2a090f294b02a88b
cepardov@gmail.com <cepardov@gmail.com@878a9883-f9bb-5e60-a21f-57833b7b4551>
2014-08-28 14:01:54 +0000

    Mantenedor de Usuario Correccion de errores

--- trunk/ReservasHotel/src/models/view/MantenedorUsuario.java
+++ trunk/ReservasHotel/src/models/view/MantenedorUsuario.java
@@ -433,19 +433,14 @@ public class MantenedorUsuario extends javax.swing.JFrame {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MantenedorUsuario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MantenedorUsuario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MantenedorUsuario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(MantenedorUsuario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 new MantenedorUsuario().setVisible(true);
             }
